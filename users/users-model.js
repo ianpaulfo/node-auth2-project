493379a140bb707796bd5a58add4a7261903a45f
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-06-11 19:15:07 -0700

    setup auth-router and user-router / MVP

---
+++ users/users-model.js
@@ -0,0 +1,34 @@
+//set up database object
+const db = require('../data/db-config');
+
+
+//find users
+function find(){
+    return db('users').select('id', 'username', 'password');
+}
+
+//find users through a specific filter
+function findBy(filter){
+    return db('users').where(filter);
+}
+
+//add users with async
+async function add(user) {
+    const [id] = await db('users').insert(user, 'id');
+
+    return findById(id);
+}
+
+//find user by id
+function findById(id) {
+    return db('users').where({id}).first();
+}
+
+module.exports ={
+
+    find,
+    findBy,
+    findById,
+    add
+
+} 
